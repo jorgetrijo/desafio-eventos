26be401cfcdcb63a42777abdaa7e5ade025e28b3
Jorge Trijo <trijojorge1@gmail.com>
2022-08-05 19:57:49 -0300

    desafio arrays

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-let jean1 = 2599;
-let jean2 = 2999;
-let jean3 = 2499;
-let jean4 = 3999;
+const jean1 = { producto: "jean", color: "clasico", marca: "Levis", precio: 2599};
+const jean2 = { producto: "jean", color: "azul oscuro", marca: "Kevingston", precio: 2999};
+const jean3 = { producto: "jean", color: "celeste", marca: "Zara", precio: 2499};
+const jean4 = { producto: "jean", color: "azul claro", marca: "McOwens", precio: 3999};
 
+const Jeans = [jean1, jean2, jean3, jean4]
+console.log("Bienvenido a Gramar Indumentarias.")
+let accion = prompt("Querés comprar o ver un detalle de los productos?")
+if (accion == "comprar"){
 let cantidad = parseInt(prompt("Cuántos jeans querés comprar? Compra máxima 2 jeans"));
 if (cantidad == 1){
     let elegido = parseInt(prompt("Qué jean te gustaria comprar? Cada jean tiene su numero asignado"))
     if (elegido == 1){
-        console.log("El jean te cuesta $" + jean1)
+        console.log("El jean te cuesta $" + jean1.precio)
     }
     else if (elegido == 2){
-        console.log("El jean te cuesta $" + jean2)
+        console.log("El jean te cuesta $" + jean2.precio)
     }
     else if (elegido == 3){
-        console.log("El jean te cuesta $" + jean3)
+        console.log("El jean te cuesta $" + jean3.precio)
     }
     else if (elegido == 4){
-        console.log("El jean te cuesta $" + jean4)
+        console.log("El jean te cuesta $" + jean4.precio)
     }
     else {
         console.log("No te entendi. Volve a empezar")
@@ -26,55 +30,59 @@ if (cantidad == 1){
 else if (cantidad == 2){
     let elegido = (prompt("Qué jeans te gustaria comprar? Compra máxima 2 jeans."))
     if (elegido == "1 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1+jean2));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean2.precio));
     }
     else if (elegido == "1 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1+jean1));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean1.precio));
     }
     else if (elegido == "1 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1+jean3));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean3.precio));
     }
     else if (elegido == "1 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1+jean4));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean4.precio));
     }
     else if (elegido == "2 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2+jean3));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean3.precio));
     }
     else if (elegido == "2 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2+jean1));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean1.precio));
     }
     else if (elegido == "2 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2+jean2));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean2.precio));
     }
     else if (elegido == "2 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2+jean4));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean4.precio));
     }
     else if (elegido == "3 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3+jean4));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean4.precio));
     }
     else if (elegido == "3 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3+jean1));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean1.precio));
     }
     else if (elegido == "3 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3+jean2));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean2.precio));
     }
     else if (elegido == "3 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3+jean3));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean3.precio));
     }
     else if (elegido == "4 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4+jean1));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean1.precio));
     }
     else if (elegido == "4 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4+jean2));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean2.precio));
     }
     else if (elegido == "4 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4+jean3));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean3.precio));
     }
     else if (elegido == "4 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4+jean4));
+        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean4.precio));
     }
     else {
         console.log("No te entendi. Volve a empezar")
     }
 }
-
+}
+else {
+    Jeans.pop ()
+    console.log(Jeans)
+}
