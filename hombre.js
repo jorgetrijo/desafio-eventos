28fe23a9b9a65b2bf36fad7b653e77fbe4e3efcb
Jorge Trijo <trijojorge1@gmail.com>
2022-08-18 20:39:46 -0300

    desafio eventos

--- hombre.js
+++ hombre.js
@@ -1,97 +1,121 @@
-const jean1 = { producto: "jean", color: "clasico", marca: "Levis", precio: 2599};
-const jean2 = { producto: "jean", color: "azul oscuro", marca: "Kevingston", precio: 2999};
-const jean3 = { producto: "jean", color: "celeste", marca: "Zara", precio: 2499};
-const jean4 = { producto: "jean", color: "azul claro", marca: "McOwens", precio: 3999};
+const productos = [
+    {
+        id: 1,
+        nombre: "Jean Levis",
+        precio: 2599,
+        imagen: "https://imagesa1.lacoste.com/dw/image/v2/BCWL_PRD/on/demandware.static/-/Sites-master/default/dwfae42a71/HH5047_MK9_24.jpg?imwidth=915&impolicy=product",
+    },
+    {
+        id: 2,
+        nombre: "Jean Kevinston",
+        precio: 2999,
+        imagen: "https://imagesa1.lacoste.com/dw/image/v2/BCWL_PRD/on/demandware.static/-/Sites-master/default/dw17e28082/HH5047_ML4_24.jpg?imwidth=915&impolicy=product",
+    },
+    {
+        id: 3,
+        nombre: "Jean Zara",
+        precio: 2499,
+        imagen: "https://imagesa1.lacoste.com/dw/image/v2/BCWL_PRD/on/demandware.static/-/Sites-master/default/dw8c9b3b49/HH9570_9LM_24.jpg?imwidth=915&impolicy=product",
+    },
+    {
+        id: 4,
+        nombre: "Jean McOwens",
+        precio: 3999,
+        imagen: "https://imagesa1.lacoste.com/dw/image/v2/BCWL_PRD/on/demandware.static/-/Sites-master/default/dw6f0ff881/HH9362_MK9_24.jpg?imwidth=915&impolicy=product",
+    },
+]
 
-const Jeans = [jean1, jean2, jean3, jean4]
-console.log("Bienvenido a Gramar Indumentarias.")
-let accion = prompt("Querés comprar o ver un detalle de los productos? Escribi comprar o detalle")
-if (accion == "comprar"){
-let cantidad = parseInt(prompt("Cuántos jeans querés comprar? Compra máxima 2 jeans"));
-if (cantidad == 1){
-    let elegido = parseInt(prompt("Qué jean te gustaria comprar? Cada jean tiene su numero asignado"))
-    if (elegido == 1){
-        console.log("El jean te cuesta $" + jean1.precio)
-    }
-    else if (elegido == 2){
-        console.log("El jean te cuesta $" + jean2.precio)
-    }
-    else if (elegido == 3){
-        console.log("El jean te cuesta $" + jean3.precio)
-    }
-    else if (elegido == 4){
-        console.log("El jean te cuesta $" + jean4.precio)
-    }
-    else {
-        console.log("No te entendi. Volve a empezar")
-    }
-    
-}
-else if (cantidad == 2){
-    let elegido = (prompt("Qué jeans te gustaria comprar? Compra máxima 2 jeans."))
-    if (elegido == "1 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean2.precio));
-    }
-    else if (elegido == "1 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean1.precio));
-    }
-    else if (elegido == "1 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean3.precio));
-    }
-    else if (elegido == "1 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean4.precio));
-    }
-    else if (elegido == "2 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean3.precio));
-    }
-    else if (elegido == "2 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean1.precio));
-    }
-    else if (elegido == "2 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean2.precio));
-    }
-    else if (elegido == "2 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean4.precio));
-    }
-    else if (elegido == "3 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean4.precio));
-    }
-    else if (elegido == "3 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean1.precio));
-    }
-    else if (elegido == "3 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean2.precio));
-    }
-    else if (elegido == "3 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean3.precio));
-    }
-    else if (elegido == "4 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean1.precio));
-    }
-    else if (elegido == "4 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean2.precio));
-    }
-    else if (elegido == "4 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean3.precio));
-    }
-    else if (elegido == "4 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean4.precio));
-    }
-    else {
-        console.log("No te entendi. Volve a empezar")
-    }
-}
-}
-else {
-    console.log("Estos son los jeans que tenemos actualmente disponibles:")
-    for (const producto of Jeans) {
-        console.log(producto)
-    }
-}
-console.log("Gracias por visitar Gramar Indumentarias.");
+let contadorCarrito = 0;
+const carrito = [];
 
-let MaximoValor = parseInt(prompt("Cuanto te gustaría gastar?"))
-console.log("Estos son los jeans que cuestan menos de $" + MaximoValor + ":")
-const buscado = Jeans.filter(producto => producto.precio < MaximoValor)
-console.log(buscado)
+const productoCatalogoHTML = (producto) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <img
+          src="${producto.imagen}"
+          class="card-img-top"
+        />
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <p class="card-text">Precio: ${producto.precio}</p>
+          <button id="btn-catalogo-${producto.id}" class="btn btn-success">Agregar</button>
+        </div>
+      </div>
+    </div>`;
+};
 
+const productoCarritoHTML = (producto) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <button id="btn-carrito-${producto.idCompra}" class="btn btn-danger">Quitar</button>
+        </div>
+      </div>
+    </div>`;
+};
+
+const mostrarCatalogo = () => {
+  const catalogoNodo = document.getElementById("catalogo");
+  let catalogoHTML = "";
+
+  for (const producto of productos) {
+    catalogoHTML += productoCatalogoHTML(producto);
+  }
+
+  catalogoNodo.innerHTML = catalogoHTML;
+  botonesCatalogo();
+};
+const mostrarCarrito = () => {
+  const carritoNodo = document.getElementById("carrito");
+  const precioNodo = document.getElementById("precioTotal");
+
+  let carritoHTML = "";
+  let precio = 0;
+  for (const producto of carrito) {
+    carritoHTML += productoCarritoHTML(producto);
+    precio += producto.precio;
+  }
+
+  precioNodo.innerHTML = precio;
+  carritoNodo.innerHTML = carritoHTML;
+  botonesCarrito();
+};
+
+const botonesCatalogo = () => {
+  for (const producto of productos) {
+    const botonId = `btn-catalogo-${producto.id}`;
+    const botonNodo = document.getElementById(botonId);
+
+    botonNodo.addEventListener("click", () => {
+      const productoCarrito = {
+        nombre: producto.nombre,
+        idCompra: contadorCarrito,
+        precio: producto.precio,
+      };
+
+      contadorCarrito += 1;
+      carrito.push(productoCarrito);
+      mostrarCarrito();
+    });
+  }
+};
+
+const botonesCarrito = () => {
+  for (const producto of carrito) {
+    const botonId = `btn-carrito-${producto.idCompra}`;
+    const botonNodo = document.getElementById(botonId);
+
+    botonNodo.addEventListener("click", () => {
+      const index = carrito.findIndex((p) => p.idCompra == producto.idCompra);
+      carrito.splice(index, 1);
+      mostrarCarrito();
+    });
+  }
+};
+
+mostrarCatalogo();
+
+console.log(productoCatalogoHTML(productos[0]));
--- mujer.js
+++ mujer.js
@@ -3,95 +3,124 @@ const jean2 = { producto: "JEAN STRAIGHT MALIBU", color: "azul oscuro", marca: "
 const jean3 = { producto: "JEAN MOM UNITED", color: "azul medio", marca: "47 street", precio: 10999};
 const jean4 = { producto: "JEAN H RELAX TADOW", color: "azul claro", marca: "47 street", precio: 9999};
 
-const Jeans = [jean1, jean2, jean3, jean4]
-console.log("Bienvenido a Gramar Indumentarias.")
-let accion = prompt("Querés comprar o ver un detalle de los productos? Escribi comprar o detalle")
-if (accion == "comprar"){
-let cantidad = parseInt(prompt("Cuántos jeans querés comprar? Compra máxima 2 jeans"));
-if (cantidad == 1){
-    let elegido = parseInt(prompt("Qué jean te gustaria comprar? Cada jean tiene su numero asignado"))
-    if (elegido == 1){
-        console.log("El jean te cuesta $" + jean1.precio)
-    }
-    else if (elegido == 2){
-        console.log("El jean te cuesta $" + jean2.precio)
-    }
-    else if (elegido == 3){
-        console.log("El jean te cuesta $" + jean3.precio)
-    }
-    else if (elegido == 4){
-        console.log("El jean te cuesta $" + jean4.precio)
-    }
-    else {
-        console.log("No te entendi. Volve a empezar")
-    }
-    
-}
-else if (cantidad == 2){
-    let elegido = (prompt("Qué jeans te gustaria comprar? Compra máxima 2 jeans."))
-    if (elegido == "1 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean2.precio));
-    }
-    else if (elegido == "1 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean1.precio));
-    }
-    else if (elegido == "1 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean3.precio));
-    }
-    else if (elegido == "1 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean1.precio+jean4.precio));
-    }
-    else if (elegido == "2 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean3.precio));
-    }
-    else if (elegido == "2 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean1.precio));
-    }
-    else if (elegido == "2 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean2.precio));
-    }
-    else if (elegido == "2 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean2.precio+jean4.precio));
-    }
-    else if (elegido == "3 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean4.precio));
-    }
-    else if (elegido == "3 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean1.precio));
-    }
-    else if (elegido == "3 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean2.precio));
-    }
-    else if (elegido == "3 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean3.precio+jean3.precio));
-    }
-    else if (elegido == "4 y 1"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean1.precio));
-    }
-    else if (elegido == "4 y 2"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean2.precio));
-    }
-    else if (elegido == "4 y 3"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean3.precio));
-    }
-    else if (elegido == "4 y 4"){
-        console.log("Los jeans te cuestan en total $" + parseInt(jean4.precio+jean4.precio));
-    }
-    else {
-        console.log("No te entendi. Volve a empezar")
-    }
-}
-}
-else {
-    console.log("Estos son los jeans que tenemos actualmente disponibles:")
-    for (const producto of Jeans) {
-        console.log(producto)
-    }
-}
-console.log("Gracias por visitar Gramar Indumentarias.");
+const productos = [
+    {
+        id: 1,
+        nombre: "JEAN L STRAIGHT HEAT",
+        precio: 11999,
+        imagen: "https://street47.vtexassets.com/arquivos/ids/189294-1200-auto?v=637859758049230000&width=1200&height=auto&aspect=true",
+    },
+    {
+        id: 2,
+        nombre: "JEAN STRAIGHT MALIBU",
+        precio: 12999,
+        imagen: "https://street47.vtexassets.com/arquivos/ids/192991-1200-auto?v=637943731604300000&width=1200&height=auto&aspect=true",
+    },
+    {
+        id: 3,
+        nombre: "JEAN MOM UNITED",
+        precio: 10999,
+        imagen: "https://street47.vtexassets.com/arquivos/ids/185301-1200-auto?v=637840889880300000&width=1200&height=auto&aspect=true",
+    },
+    {
+        id: 4,
+        nombre: "JEAN H RELAX TADOW",
+        precio: 9999,
+        imagen: "https://street47.vtexassets.com/arquivos/ids/180721-1200-auto?v=637827036879770000&width=1200&height=auto&aspect=true",
+    },
+]
 
-let MaximoValor = parseInt(prompt("Cuanto te gustaría gastar?"))
-console.log("Estos son los jeans que cuestan menos de $" + MaximoValor + ":")
-const buscado = Jeans.filter(producto => producto.precio < MaximoValor)
-console.log(buscado)
+let contadorCarrito = 0;
+const carrito = [];
 
+const productoCatalogoHTML = (producto) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <img
+          src="${producto.imagen}"
+          class="card-img-top"
+        />
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <p class="card-text">Precio: ${producto.precio}</p>
+          <button id="btn-catalogo-${producto.id}" class="btn btn-success">Agregar</button>
+        </div>
+      </div>
+    </div>`;
+};
+
+const productoCarritoHTML = (producto) => {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <button id="btn-carrito-${producto.idCompra}" class="btn btn-danger">Quitar</button>
+        </div>
+      </div>
+    </div>`;
+};
+
+const mostrarCatalogo = () => {
+  const catalogoNodo = document.getElementById("catalogo");
+  let catalogoHTML = "";
+
+  for (const producto of productos) {
+    catalogoHTML += productoCatalogoHTML(producto);
+  }
+
+  catalogoNodo.innerHTML = catalogoHTML;
+  botonesCatalogo();
+};
+const mostrarCarrito = () => {
+  const carritoNodo = document.getElementById("carrito");
+  const precioNodo = document.getElementById("precioTotal");
+
+  let carritoHTML = "";
+  let precio = 0;
+  for (const producto of carrito) {
+    carritoHTML += productoCarritoHTML(producto);
+    precio += producto.precio;
+  }
+
+  precioNodo.innerHTML = precio;
+  carritoNodo.innerHTML = carritoHTML;
+  botonesCarrito();
+};
+
+const botonesCatalogo = () => {
+  for (const producto of productos) {
+    const botonId = `btn-catalogo-${producto.id}`;
+    const botonNodo = document.getElementById(botonId);
+
+    botonNodo.addEventListener("click", () => {
+      const productoCarrito = {
+        nombre: producto.nombre,
+        idCompra: contadorCarrito,
+        precio: producto.precio,
+      };
+
+      contadorCarrito += 1;
+      carrito.push(productoCarrito);
+      mostrarCarrito();
+    });
+  }
+};
+
+const botonesCarrito = () => {
+  for (const producto of carrito) {
+    const botonId = `btn-carrito-${producto.idCompra}`;
+    const botonNodo = document.getElementById(botonId);
+
+    botonNodo.addEventListener("click", () => {
+      const index = carrito.findIndex((p) => p.idCompra == producto.idCompra);
+      carrito.splice(index, 1);
+      mostrarCarrito();
+    });
+  }
+};
+
+mostrarCatalogo();
+
+console.log(productoCatalogoHTML(productos[0]));
